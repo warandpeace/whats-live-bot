e793c33ddd4a8b2dc14111d8a14b87cfcfbd2333
Andrew Ferguson <cabbit@goldengod.net>
2017-04-10 21:32:58 -0700

    something fucky with my tabbing?

--- functions/simple/main.go
+++ functions/simple/main.go
@@ -2,59 +2,58 @@ package main
 
 // Importing dependencies
 import (
-  "encoding/json"
-  "io/ioutil"
-  "log"
-  "os"
-  "github.com/apex/go-apex"
-  "github.com/ChimeraCoder/anaconda"
+	"encoding/json"
+	"github.com/ChimeraCoder/anaconda"
+	"github.com/apex/go-apex"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 // Creating credentials structure
 type credentials struct {
-    CtwitterKey         string `json:"twitterKey"`
-    CtwitterSecret      string `json:"twitterSecret"`
-    CtwitterToken       string `json:"twitterToken"`
-    CtwitterTokenSecret string `json:"twitterTokenSecret"`
+	CtwitterKey         string `json:"twitterKey"`
+	CtwitterSecret      string `json:"twitterSecret"`
+	CtwitterToken       string `json:"twitterToken"`
+	CtwitterTokenSecret string `json:"twitterTokenSecret"`
 }
 
 // Creating message structure (Note: From Apex Golang Example)
 // TODO: Remove this at some point
 type message struct {
-  Hello string `json:"hello"`
+	Hello string `json:"hello"`
 }
 
-// I don't fucking know why I'm doing this but if I don't things break.
-// It looks like just an aliasing, but the json.Unmarshal refused '&credentials' so shrug emoji
+// Hooray rakslice explained this and now I know why I'm doing it!
+// It's because I've defined credentials above but I still need to _declare_ an instance of it
 var cred credentials
 
 // Using the init function to read in my totally insecure credentials json file
 // and set all my credentials for API calls in main()
 func init() {
-  file, err := ioutil.ReadFile("./credentials.json")
-  if err != nil {
-    log.Printf("File error: %v\n", err)
-    os.Exit(1)
-  }
-  json.Unmarshal(file, &cred)
-  // Set Anaconda credentials
-  // TODO: Verify these are being set properly with the values from the local json file
-  anaconda.SetConsumerKey(cred.CtwitterKey)
-  anaconda.SetConsumerSecret(cred.CtwitterSecret)
-  // api := anaconda.NewTwitterApi(cred.CtwitterToken, cred.CtwitterTokenSecret)
+	file, err := ioutil.ReadFile("./credentials.json")
+	if err != nil {
+		log.Printf("File error: %v\n", err)
+		os.Exit(1)
+	}
+	json.Unmarshal(file, &cred)
+	// Set Anaconda credentials
+	// TODO: Verify these are being set properly with the values from the local json file
+	anaconda.SetConsumerKey(cred.CtwitterKey)
+	anaconda.SetConsumerSecret(cred.CtwitterSecret)
+	// api := anaconda.NewTwitterApi(cred.CtwitterToken, cred.CtwitterTokenSecret)
 }
 
 // It's the main function! Where it does... stuff! (Note: From Apex Golang Example)
 // TODO: Fucking everything. Replace this at bare minimum with a hello world tweet to get started.
 func main() {
-  apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
-    var m message
+	apex.HandleFunc(func(event json.RawMessage, ctx *apex.Context) (interface{}, error) {
+		var m message
 
-    if err := json.Unmarshal(event, &m); err != nil {
-      return nil, err
-    }
+		if err := json.Unmarshal(event, &m); err != nil {
+			return nil, err
+		}
 
-    return m, nil
-  })
+		return m, nil
+	})
 }
-
